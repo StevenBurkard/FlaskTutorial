74ca87d01eb1c511939563b317b4cc55e76d271c
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-06-16 14:52:16 -0500

    updated app.py to create a model which is located in the Car class

--- app.py
+++ app.py
@@ -23,8 +23,14 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    make = db.Column(db.String(255), nullable = False)
+    model = db.Column(db.String(255), nullable = False)
+    year = db.Column(db.Integer)
 
-
+    def __repr__(self):
+        return f'{self.year} {self.make} {self.model}'
 
 # Schemas
 
